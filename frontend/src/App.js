fdbfe05133fa4cdaacf1812d2a5a63a88d21a703
bestemic <przemek.pawlik123@gmail.com>
2022-03-04 13:08:25 +0100

    Adding navbar with menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import {Container} from "react-bootstrap";
 import ListTaskComponent from "./components/ListTaskComponent";
+import HeaderComponent from "./components/HeaderComponent";
 
 function App() {
     return (
-        <Container>
-            <ListTaskComponent/>
+        <Container id="root" fluid>
+            <HeaderComponent/>
+            <Container>
+                <ListTaskComponent/>
+            </Container>
         </Container>
     );
 }
---
+++ frontend/src/components/HeaderComponent.js
@@ -0,0 +1,22 @@
+import {Container, Nav, Navbar} from "react-bootstrap";
+
+
+function HeaderComponent(){
+    return(
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <Container>
+                    <Navbar.Brand>Todo Application</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+}
+
+export default HeaderComponent;
