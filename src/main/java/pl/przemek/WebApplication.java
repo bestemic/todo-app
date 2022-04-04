053cbe5bfeebdf5245c023ca9a41fa8aa188aa0f
bestemic <przemek.pawlik123@gmail.com>
2022-04-04 11:28:33 +0200

    maven initialization

--- src/main/java/pl/przemek/WebApplication.java
+++ src/main/java/pl/przemek/WebApplication.java
@@ -1,5 +1,4 @@
 package pl.przemek;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
